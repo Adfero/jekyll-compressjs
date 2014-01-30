a821a1b01a38df31f7b4640d00ad28163ae4f9b9
John Jones <JJones@adfero.com>
2014-01-30 11:43:19 -0500

    Added checking for "watch" or "build" state to guide minification

--- compressjs.rb
+++ compressjs.rb
@@ -1,59 +1,60 @@
 module Jekyll
-  class CompressJSFile < StaticFile
-    def write(dest)
-      # do nothing
-    end
-  end
+	class CompressJSFile < StaticFile
+		def write(dest)
+			# do nothing
+		end
+	end
 
-  class CompressJSGenerator < Generator
-    safe true
+	class CompressJSGenerator < Generator
+		safe true
 
-    def generate(site)
-      config = Jekyll::CompressJSGenerator.get_config
-      
-      last_modified = File.mtime(config['js_main'])
+		def generate(site)
+			config = Jekyll::CompressJSGenerator.get_config
+			
+			last_modified = File.mtime(config['js_main'])
 
-      output_dir = File.join(site.config['destination'], config['js_destination_dir'])
-      output_file = File.join(output_dir, config['js_destination_file'])
+			output_dir = File.join(site.config['destination'], config['js_destination_dir'])
+			output_file = File.join(output_dir, config['js_destination_file'])
 
-      # need to create destination dir if it doesn't exist
-      FileUtils.mkdir_p(output_dir)
-      minify_js(config['js_main'], output_file)
-      site.static_files << CompressJSFile.new(site, site.source, config['js_destination_dir'], config['js_destination_file'])
-    end
+			# need to create destination dir if it doesn't exist
+			FileUtils.mkdir_p(output_dir)
+			minify_js(config['js_main'], output_file, site.config.has_key?('watch') == false)
+			site.static_files << CompressJSFile.new(site, site.source, config['js_destination_dir'], config['js_destination_file'])
+		end
 
-    def minify_js(js_file, output_file)
-      juice_cmd = "juicer merge --force -s -i #{js_file} -o #{output_file}"
-      puts juice_cmd
-      system(juice_cmd)
-    end
+		def minify_js(js_file, output_file, minify)
+			minify = minify ? "" : "--minifyer \"\""
+			juice_cmd = sprintf "juicer merge %s --force -s -i #{js_file} -o #{output_file}", minify
+			puts juice_cmd
+			system(juice_cmd)
+		end
 
-    def self.get_config
-      if @config == nil
-        @config = {
-          'js_main' => '_js/main.js',
-          'js_destination_dir' => '/js',
-          'js_destination_file' => 'main.min.js'
-        }
-        config = YAML.load_file('CompressJS.yml') rescue nil
-        if config.is_a?(Hash)
-          @config = @config.merge(config)
-        end
-      end
-      return @config
-    end
-  end
+		def self.get_config
+			if @config == nil
+				@config = {
+					'js_main' => '_js/main.js',
+					'js_destination_dir' => '/js',
+					'js_destination_file' => 'main.min.js'
+				}
+				config = YAML.load_file('CompressJS.yml') rescue nil
+				if config.is_a?(Hash)
+					@config = @config.merge(config)
+				end
+			end
+			return @config
+		end
+	end
 
-  class CompressJSLinkTag < Liquid::Tag
-    def initialize(tag_name, text, tokens)
-      super
-    end
+	class CompressJSLinkTag < Liquid::Tag
+		def initialize(tag_name, text, tokens)
+			super
+		end
 
-    def render(context)
-      config = Jekyll::CompressJSGenerator.get_config
-      File.join(config['js_destination_dir'],config['js_destination_file'])
-    end
-  end
+		def render(context)
+			config = Jekyll::CompressJSGenerator.get_config
+			File.join(config['js_destination_dir'],config['js_destination_file'])
+		end
+	end
 end
 
 Liquid::Template.register_tag('minified_js_file', Jekyll::CompressJSLinkTag)
